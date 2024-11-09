fdf0ae972f219306c132bc44149ab91d9adf57e9
MateuszKretkowski <mkretkowski13@gmail.com>
2024-11-09 20:08:48 +0100

    Added couple functions to PixelMapProcessor.cs

--- Assets/Scripts/PixelMapProcessor.cs
+++ Assets/Scripts/PixelMapProcessor.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class PixelMapProcessor
 {
+    PixelMap pixelMap;
 
+    public PixelMapProcessor(PixelMap pixelMap)
+    {
+        this.pixelMap = pixelMap;
+    }
+
+    public void AddColorData(Vector2 pos, Color32 color, int dictionaryIndex)
+    {
+        if (dictionaryIndex < 0 || dictionaryIndex >= pixelMap.colorData.Count)
+        {
+            Debug.LogError("Invalid dictionary index.");
+            return;
+        }
+        pixelMap.colorData[dictionaryIndex][pos] = color;
+    }
+
+    public void AddDictionary()
+    {
+        pixelMap.colorData.Add(new Dictionary<Vector2, Color32>());
+    }
 }
