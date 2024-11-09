4e272659df7aafcf81edad78fe6da06db703c57e
MateuszKretkowski <mkretkowski13@gmail.com>
2024-11-09 17:51:57 +0100

    Setting everything up

---
+++ Assets/Scripts/PixelMap.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelMap : ScriptableObject
+{
+    public List<Dictionary<Vector2, Color32>> colorData;
+}
